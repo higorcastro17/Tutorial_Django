34e8db3d6f53471e1bd1cad1eeecb4a17754ff02
Carmosoeles <carmosoeles16@gmail.com>
2020-02-10 15:59:09 -0300

    Atualizando

--- meusite/urls.py
+++ meusite/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from votacao.views import index
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('votacao/', include('votacao.urls'))
+   
 ]
---
+++ votacao/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from votacao.views import index, resultados
+
+urlpatterns = [
+    path('', index),
+    path('votar/', index),
+    path('resultados/', resultados),
+
+   
+]
